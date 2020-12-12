227647628339ac33be711d6e0b05bb904602247e
Jakub Lunarzewski <jakub.lunarzewski@gmail.com>
2020-12-12 18:43:16 +1100

    Initial controller setup for testing

---
+++ luna-projects.Api/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using luna_projects_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace luna_projects_api.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            var person = new Person
+            {
+                FirstName = "Johnny",
+                LastName = "BeGoode",
+                Age = 32
+            };
+            return Ok(person);
+        }
+    }
+}
---
+++ luna-projects.Api/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace luna_projects_api.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
